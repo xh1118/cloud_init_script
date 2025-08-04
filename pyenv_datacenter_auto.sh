32206afd117e59160e4a24db6d46ae3895032c20
xh1118 <89822520+xh1118@users.noreply.github.com>
2025-08-04 22:30:52 +0800

    Update pyenv_datacenter_auto.sh

--- pyenv_datacenter_auto.sh
+++ pyenv_datacenter_auto.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
-# 下载 datacenter.json
-wget -O datacenter.json https://raw.githubusercontent.com/xh1118/cloud_init_script/main/pyenv_datacenter.json
+# 确认是否需要创建目录
+if [ -d "$HOME/json" ]; then
+    echo "json目录已存在，跳过创建"
+else
+    echo "创建json目录..."
+    mkdir -p $HOME/json
+fi
 
-# 创建目录（只需要datacenter目录）
-mkdir -p $HOME/json
-
-# 复制到指定路径
-cp datacenter.json $HOME/json/pyenv_datacenter.json
+# 直接下载到目标目录
+wget -O $HOME/json/pyenv_datacenter.json https://raw.githubusercontent.com/xh1118/cloud_init_script/main/pyenv_datacenter.json
 
 # 启动PM2（logs目录会自动创建）
 pm2 start $HOME/json/pyenv_datacenter.json
 
 # 保存配置
-pm2 save 
+pm2 save
